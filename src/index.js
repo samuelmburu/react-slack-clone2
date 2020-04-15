17434f4f0eddcf8c09c2b97280ecb5b41753074f
Samuel Mburu <samuelmburu@Samuels-MacBook-Pro.local>
2020-04-14 21:26:32 -0700

    wiring up redux -- rough cut

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter,Router, Switch, Route, withRouter } from 'react-router-dom';
 
+import {createStore, compose } from 'redux';
+import {Provider} from 'react-redux';
+
 import 'semantic-ui-css/semantic.min.css';
 
 import firebase from './firebase';
@@ -9,6 +12,26 @@ import { App } from './App';
 import { Login } from './components/Auth/Login';
 import { Register } from './components/Auth/Register';
 import * as serviceWorker from './serviceWorker';
+
+let composeEnhancers = compose;
+
+// If Redux Dev Tools and Saga Dev Tools Extensions are installed, enable them
+/* istanbul ignore next */
+if (process.env.NODE_ENV !== 'production' && typeof window === 'object') {
+  /* eslint-disable no-underscore-dangle */
+  if (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({});
+
+  // NOTE: Uncomment the code below to restore support for Redux Saga
+  // Dev Tools once it supports redux-saga version 1.x.x
+  // if (window.__SAGA_MONITOR_EXTENSION__)
+  //   reduxSagaMonitorOptions = {
+  //     sagaMonitor: window.__SAGA_MONITOR_EXTENSION__,
+  //   };
+  /* eslint-enable */
+}
+
+const store = createStore(() => {}, {}, composeEnhancers());
 
 function Root({history}) {
   useEffect(()=> {
@@ -32,7 +55,13 @@ function Root({history}) {
 
 const RootWithAuthRouting = withRouter(Root);
 
-ReactDOM.render(<BrowserRouter><RootWithAuthRouting /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <RootWithAuthRouting />
+    </BrowserRouter>
+  </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
